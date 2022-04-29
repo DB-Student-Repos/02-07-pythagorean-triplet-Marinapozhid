129cb116c6a720792c525698ca7ac3f064605e35
Marina <m.pozhidayeva@gmail.com>
2022-04-29 11:53:27 +0200

    raw_solution

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -1,2 +1,12 @@
 def triplets_with_sum(number):
-    pass
+    triplets = []
+    for a in range(number):
+        for c in range(number):
+            b = number - a - c
+            if a ** 2 + b ** 2 == c ** 2 and a < b < c:
+                triplets.append([a, b, c])
+    return triplets
+
+
+
+
