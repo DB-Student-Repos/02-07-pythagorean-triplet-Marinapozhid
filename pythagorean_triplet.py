cf63343e35282730ec7d114b6f1e4ab55a2b1a78
Marina <m.pozhidayeva@gmail.com>
2022-05-05 11:46:08 +0200

    takes_too_long

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -1,9 +1,11 @@
+import math
 def triplets_with_sum(number):
+
     triplets = []
-    for a in range(number):
-        for c in range(number):
-            b = number - a - c
-            if a ** 2 + b ** 2 == c ** 2 and a < b < c:
+    for b in range(number):
+        for a in range(1, b):
+            c = math.sqrt(a ** 2 + b ** 2)
+            if a + b + c == number:
                 triplets.append([a, b, c])
     return triplets 
 
