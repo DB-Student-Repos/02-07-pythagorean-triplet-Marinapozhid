b422363b07f958b937755bc3a7f4b046ef9b027e
Marina <m.pozhidayeva@gmail.com>
2022-05-23 11:22:52 +0200

    more_math_sol

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -1,14 +1,19 @@
-import math
 def triplets_with_sum(number):
 
     triplets = []
-    for b in range(number):
-        for a in range(1, b):
-            c = math.sqrt(a ** 2 + b ** 2)
-            if a + b + c == number:
-                triplets.append([a, b, c])
-    return triplets 
-
-
+    for a in range(1, number - 3):
+        ''' b^2 = c^2 - a^2 and c = number - a - b 
+        b^2 = (number - a - b)^2 - a^2
+        b^2 = (number - a)^2 - 2(number - a)b + b^2 - a^2
+        number^2 - 2number*a + a^2 - 2b*number + 2ab - a^2 = 0
+        b(2number - 2a) = number^2 - 2a*number
+        
+        type(b) == int
+        '''
+        b = (number ** 2 - 2 * number * a) // (2 * number - 2 * a)
+        c = number - a - b
+        if a ** 2 + b ** 2 == c ** 2 and c > b and b > a:
+                triplets.append([a,b,c])
+    return triplets
 
 
