f867eda6b11fa3f0cb50c8b699513771afe0e749
Marina <m.pozhidayeva@gmail.com>
2022-05-24 15:51:15 +0200

    list_compr_likeinthebook

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -1,19 +1,10 @@
+from math import sqrt
 def triplets_with_sum(number):
+    triplets = [
+        (a, b, sqrt(a**2 + b**2)) for a in range(1, number // 3) for b in range((a + 1), number)
+        ]
+    triplets = list(filter(lambda triple: triple[2].is_integer(), triplets))
 
-    triplets = []
-    for a in range(1, number // 3):
-        ''' b^2 = c^2 - a^2 and c = number - a - b 
-        b^2 = (number - a - b)^2 - a^2
-        b^2 = (number - a)^2 - 2(number - a)b + b^2 - a^2
-        number^2 - 2number*a + a^2 - 2b*number + 2ab - a^2 = 0
-        b(2number - 2a) = number^2 - 2a*number
-        
-        type(b) == int
-        '''
-        b = (number ** 2 - 2 * number * a) // (2 * number - 2 * a)
-        c = number - a - b
-        if a ** 2 + b ** 2 == c ** 2 and c > b and b > a:
-                triplets.append([a,b,c])
-    return triplets
+    return list(filter(lambda triple: sum(triple) == number, triplets))
 
 
